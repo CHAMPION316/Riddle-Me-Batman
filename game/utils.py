77b7a96b2b13d7d0b3321739db9e6d562215f5e5
C316 <royer.seguracalderon@gmail.com>
2022-08-15 23:33:13 +0000

    Create a print delay function

--- game/utils.py
+++ game/utils.py
@@ -1,2 +1,12 @@
 import time
 import os
+
+def delay_print(text, delay):
+    """
+    Delays the print statement
+    Parameters:
+        text (string) - text to be printed out
+        delay (integer) - the number of seconds to delay by
+    """
+    print(" ", text)
+    time.sleep(delay)
