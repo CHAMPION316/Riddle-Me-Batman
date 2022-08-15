6ef225860f7fbc7941a903c1f8152a35df253aae
C316 <royer.seguracalderon@gmail.com>
2022-08-15 23:42:59 +0000

    Move code in run.py to functions.py

--- run.py
+++ run.py
@@ -1,16 +1,3 @@
-import gspread
-from google.oauth2.service_account import Credentials
+from game.functions import main
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('riddle_me_batman')
-
-high_scores = SHEET.worksheet('High_Scores')
-scores = high_scores.get_all_records()
+main()
