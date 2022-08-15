b0dd4b3d65e29f617780193f8e466d0cd6ea43f6
C316 <royer.seguracalderon@gmail.com>
2022-08-15 21:43:36 +0000

    Test that API is working

--- run.py
+++ run.py
@@ -13,3 +13,4 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('riddle_me_batman')
 
 high_scores = SHEET.worksheet('High_Scores')
+scores = high_scores.get_all_records()
