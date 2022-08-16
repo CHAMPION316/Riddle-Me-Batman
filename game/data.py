c4f2d064069ad1242c979d9e9756387b142baea4
C316 <royer.seguracalderon@gmail.com>
2022-08-16 17:08:27 +0000

    Create values variable in update_highscores_sheet

--- game/data.py
+++ game/data.py
@@ -28,6 +28,7 @@ masked_word = ''
 
 def update_highscores_sheet():
     keys = [str(eachvalue) for eachvalue in scores[0].keys()]
+    values = [str(eachvalue) for eachvalue in scores[0].values()]
 
 
 def logo():
