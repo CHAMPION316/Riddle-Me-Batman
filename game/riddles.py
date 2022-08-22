9d84995cc335f6c87dae0a5cce7cf3e3f88c99be
C316 <royer.seguracalderon@gmail.com>
2022-08-22 18:20:39 +0000

    Create riddles.py

---
+++ game/riddles.py
@@ -0,0 +1,8 @@
+# from functions.py import get_word
+
+
+# def Riddle(riddleIt):
+#     if get_word() == 'wonderwoman':
+#         print("Sword and shield, semi-god warrior of women")
+#     else:
+#         return False
