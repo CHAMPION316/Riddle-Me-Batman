46461b17c412a7a54926ed902ff5feba8a895029
C316 <royer.seguracalderon@gmail.com>
2022-08-23 19:39:41 +0000

    Create riddles array in riddes.py

--- game/riddles.py
+++ game/riddles.py
@@ -6,3 +6,52 @@
 #         print("Sword and shield, semi-god warrior of women")
 #     else:
 #         return False
+
+
+
+# riddles = [
+#     1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,27,29,30,31,32,33,34,35,36,37,38
+
+# ]
+
+# riddles = [
+#     'dark knight',
+#     'red underwear',
+#     'Goddness of Zeus',
+#     'Destruction',
+#     'Billionaire Evil Genius',
+#     'Time Traveler',
+#     'Motherbox monster',
+#     'Talk to fish?',
+#     'Detective Bat',
+#     'microscopic',
+#     'steroid freak',
+#     'Adult bat',
+#     'Evil lighting',
+#     'Chosen child',
+#     'Seas blackhole',
+#     'Concert screamer',
+#     'God of War',
+#     'ruthless boss',
+#     'caretaker',
+#     'intergalactic fighter',
+#     'never misses',
+#     'conquerer of worlds',
+#     'Pure Evil',
+#     'hitman specialist',
+#     'ghost to humans',
+#     'a bug on the wall',
+#     'ugly gangster',
+#     'red flying bird',
+#     'wings that fly at night',
+#     'wealth for the poor',
+#     'protector of earth',
+#     'a bird man',
+#     'a bird girl',
+#     'satistic prince of evil',
+#     'the princess of the prince of evil',
+#     'Heavymetal space traveler',
+#     'figher of the spirit world',
+#     'magician of beauty',
+#     'beautifully overpowered'
+# ]
